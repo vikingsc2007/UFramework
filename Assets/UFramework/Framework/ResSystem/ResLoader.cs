ae8021f07a5c7a37017ca20d6d5e1f2a3d64e0b6
代码之神罩着我 <273562225@qq.com>
2017-09-11 18:32:03 +0800

    资源系统基础框架

---
+++ Assets/UFramework/Framework/ResSystem/ResLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UFramework.Engine.Pool.Interfaces;
+using UnityEngine;
+
+namespace UFramework.Framework.ResSystem
+{
+	public class ResLoader : IPoolItem
+	{
+		public void OnReborn()
+		{
+			
+		}
+
+		public void OnCache()
+		{
+			
+		}
+
+		public bool Cached { get; set; }
+	}
+}
---
+++ Assets/UFramework/Framework/ResSystem/ResSystem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UFramework.Engine.Pool.Pools;
+using UFramework.Engine.Singleton;
+using UFramework.Framework.ResSystem.Interfaces;
+
+
+namespace UFramework.Framework.ResSystem
+{
+	
+	public class ResSystem :SingletonMonoT<ResSystem>
+	{
+
+		public static int MaxLoaderCount = 20;
+		public static int InitLoaderCount = 5;
+		
+		
+		
+		private Dictionary<string,IRes> _loadedResource;
+
+		
+		
+		
+		public override void OnInstanced()
+		{
+			ObjectPool<ResLoader>.Inst.Initalize(MaxLoaderCount,InitLoaderCount);
+			_loadedResource = new Dictionary<string, IRes>();
+		}
+
+
+
+
+		public ResLoader PickLoader()
+		{
+			return ObjectPool<ResLoader>.Inst.Pick();
+		}
+		
+		
+		public bool DropLoader(ResLoader loader)
+		{
+			return ObjectPool<ResLoader>.Inst.Drop(loader);
+		}
+
+
+	}
+}
+
